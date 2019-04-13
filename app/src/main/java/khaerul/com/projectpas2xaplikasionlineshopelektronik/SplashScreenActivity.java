4e08de41cf30f8205f1d7b08868e52a21ab2a091
rkkhaerul <rkkhaerul@gmail.com>
2019-04-13 19:44:20 +0700

    13/04/2019

---
+++ app/src/main/java/khaerul/com/projectpas2xaplikasionlineshopelektronik/SplashScreenActivity.java
@@ -0,0 +1,96 @@
+package khaerul.com.projectpas2xaplikasionlineshopelektronik;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private AVLoadingIndicatorView avi;
+    private static final String[] INDICATORS=new String[]{
+            "BallPulseIndicator",
+            "BallGridPulseIndicator",
+            "BallClipRotateIndicator",
+            "BallClipRotatePulseIndicator",
+            "SquareSpinIndicator",
+            "BallClipRotateMultipleIndicator",
+            "BallPulseRiseIndicator",
+            "BallRotateIndicator",
+            "CubeTransitionIndicator",
+            "BallZigZagIndicator",
+            "BallZigZagDeflectIndicator",
+            "BallTrianglePathIndicator",
+            "BallScaleIndicator",
+            "LineScaleIndicator",
+            "LineScalePartyIndicator",
+            "BallScaleMultipleIndicator",
+            "BallPulseSyncIndicator",
+            "BallBeatIndicator",
+            "LineScalePulseOutIndicator",
+            "LineScalePulseOutRapidIndicator",
+            "BallScaleRippleIndicator",
+            "BallScaleRippleMultipleIndicator",
+            "BallSpinFadeLoaderIndicator",
+            "LineSpinFadeLoaderIndicator",
+            "TriangleSkewSpinIndicator",
+            "PacmanIndicator",
+            "BallGridBeatIndicator",
+            "SemiCircleSpinIndicator",
+            "com.wang.avi.sample.MyCustomIndicator"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ButterKnife.bind(this);
+
+//        String indicator=getIntent().getStringExtra("indicator");
+        avi= (AVLoadingIndicatorView) findViewById(R.id.Splash);
+        avi.setIndicator("BallScaleMultipleIndicator");
+//        avi.setIndicator("PacmanIndicator");
+
+
+//        //pembuatan animasi Bounce (atas bawahh)
+//        new BounceAnimation(lv_loading)
+//                .setBounceDistance(50)
+//                .setDuration(3000)
+//                .animate();
+
+        //membuat sebuah proses yang ter delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //mendefenisikan Intent activity
+                Intent i = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(i);
+
+                //finish berguna untuk mengakhiri activity
+                //disini saya menggunakan finish,supaya ketika menekan tombol back
+                //tidak kembali pada activity SplashScreen nya
+                finish();
+            }
+            //disini maksud 3000 nya itu adalah lama screen ini terdelay 3 detik,dalam satuan mili second
+        }, 5000);
+    }
+
+    public void hideClick(View view) {
+        avi.hide();
+        // or avi.smoothToHide();
+    }
+
+    public void showClick(View view) {
+        avi.show();
+        // or avi.smoothToShow();
+    }
+}
